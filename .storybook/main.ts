56baf4f0441035d23aeffbe363d7dd50b7647be8
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2026-08-13 19:57:33 +0000

    Make storybook tests work on local again

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -41,6 +41,8 @@ const config: StorybookConfig = {
     viteConfig.plugins = [...(viteConfig.plugins ?? []), rawSrcJsPlugin]
     return viteConfig
   },
+
+  addons: ['@storybook/addon-vitest']
 }
 
 export default config
